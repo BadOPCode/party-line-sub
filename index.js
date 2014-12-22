e69c87acd584dc267e3729b49b82b7c866d29b15
BadOPCode <shawn@wmsi.co>
2014-12-22 16:12:54 +0000

    First commit

---
+++ index.js
@@ -0,0 +1,14 @@
+'use strict';
+/**
+ *  index.js - Shawn Rapp 2014-12-20
+ *  Party Line subsystem interface library bootstrap.
+ *  @author Shawn Rapp
+ *  @license MIT. Read LICENSE for more information.
+ *  @fileOverview Bootstraps library.
+ */
+
+/**
+ * Export lib/party-line-sub
+ */
+
+ module.exports = require('./lib');
